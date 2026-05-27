0ad20f3ab6856df999f2a12241aae3c0b0377889
Smart Charity Dev <dev@smart-charity.app>
2026-05-27 19:39:56 +0300

    fix: exclude /api from middleware, fix netlify.toml redirect URL

--- frontend-saas/middleware.ts
+++ frontend-saas/middleware.ts
@@ -39,6 +39,6 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|fonts|icons).*)",
+    "/((?!_next/static|_next/image|favicon.ico|fonts|icons|api).*)",
   ],
 };
